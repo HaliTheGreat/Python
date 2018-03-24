e5009136f93a19fea81ba12fcffcc5789ef880d9
Nathan Metzger <linuxfan724@gmail.com>
2018-03-24 15:56:31 -0400

    Messaging Revisions
    Working on messaging
    + Nickname

--- Server.py
+++ Server.py
@@ -1,32 +1,29 @@
 import socket
 
 
-
-
-
 def Main():
     host = '127.0.0.1'
-    port = 6667
+    port = 8675
 
     s = socket.socket()
     s.bind((host, port))
-
-    s.listen(1)
-    c, addr = s.accept()
-    print("Connection incoming: " + str(addr))
     while True:
-        data = c.recv(2048)
-        data = data.decode('utf-8')
-        StrData = str(data)
-        if not data:
-            break
-        print("from user: " + StrData)
-        data = "To the renfair: " + StrData
-        print("Sending user data: " + StrData)
-        byter = data.encode()
-        c.sendall(byter)
+        s.listen(1)
+        c, addr = s.accept()
+        print("Connection incoming: " + str(addr))
+        while True:
+            data = c.recv(2048)
+            data = data.decode('utf-8')
+            StrData = str(data)
+            if not data:
+                break
+            print("Sent message: " + StrData)
+            data = StrData
+            print("Sending user data: " + StrData)
+            byter = data.encode()
+            c.sendall(byter)
+        c.close()
     c.close()
 
 if __name__ == '__main__':
-
     Main()
--- client.py
+++ client.py
@@ -1,22 +1,25 @@
 import socket
-
+import os
 
 def Main():
+    os.system('clear')
     host = "127.0.0.1"
-    port = 6667
+    port = 8675
 
     s = socket.socket()
     s.connect((host, port))
-
-    marsuge = raw_input("Enter your data or type 'quit' to quit: ")
-    bytern = marsuge.encode()
-    while marsuge != 'quit':
+    nickname = input('Unique Identifier: ')
+    message = ""
+    while message != 'quit':
+        print('Type to send message, "quit" to quit.')
+        message = input('> ')
+        output = "<" + nickname + ">: " + message
+        bytern = output.encode()
         s.sendall(bytern)
-        data = s.recv(1024)
+        data = s.recv(2048)
         StringData = data.decode()
-        print("Server said: " + StringData)
-        marsuge = raw_input("Send another one: ")
-        bytern = marsuge.encode()
+        os.system('clear')
+        print(StringData)
     s.close()
 
 if __name__ == '__main__':
